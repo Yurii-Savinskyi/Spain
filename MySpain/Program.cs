84628a6570cd0682f3ae28f03d8a5fe6f49be0a1
Intell\ASUS <yur.savinskij@gmail.com>
2022-11-27 12:05:40 +0200

    One fullinfo

---
+++ MySpain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MySpain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Spain ( Kingdom of Spain)");
+        }
+    }
+}
