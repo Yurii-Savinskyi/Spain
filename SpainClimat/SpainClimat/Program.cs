06481c38f7679126a65c499f44d872f79123ecfd
Intell\ASUS <yur.savinskij@gmail.com>
2022-11-27 13:24:42 +0200

    One clim

---
+++ SpainClimat/SpainClimat/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SpainClimat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Mediterranean climate");
+            Console.WriteLine("The semi-arid climate ");
+        }
+    }
+}
